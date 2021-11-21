cd0d5e9274bb56d9a7379a9dffbf44ccbbb68e98
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2021-11-21 15:44:23 +0300

    Use IIFE

--- homework_8/script-star.js
+++ homework_8/script-star.js
@@ -1,22 +1,23 @@
 'use strict';
 
-let fibonacci;
-let previosElem = 0;
-let currentElem = 1;
-let n = 1;
-fibonacci = function () {
-    if (n === 1) {
-        console.log(0);
-    } else if (n === 2) {
-        console.log(1);
-    } else {
-        let nextElem = previosElem + currentElem;
-        console.log(nextElem);
-        previosElem = currentElem;
-        currentElem = nextElem;
+const fibonacci = (function () {
+    let previosElem = 0;
+    let currentElem = 1;
+    let n = 1;
+    return function () {
+        if (n === 1) {
+            console.log(0);
+        } else if (n === 2) {
+            console.log(1);
+        } else {
+            let nextElem = previosElem + currentElem;
+            console.log(nextElem);
+            previosElem = currentElem;
+            currentElem = nextElem;
+        }
+        n++;
     }
-    n++;
-}
+})();
 
 fibonacci();
 fibonacci();
